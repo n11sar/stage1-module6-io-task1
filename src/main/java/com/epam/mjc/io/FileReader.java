015d77b82fc72b0c2cab9c7d3fe9f170270978c0
georgy <ganus1111@gmail.com>
2023-02-06 18:54:46 +0200

    feat: add "FileReader" task solution

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,50 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        StringBuilder sb = new StringBuilder();
+        Profile profile = new Profile();
+
+        try(FileInputStream in = new FileInputStream(file.getAbsolutePath())) {
+
+            int read;
+
+            while ((read = in.read()) != -1) {
+                sb.append((char) read);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        String[] pairs = sb.toString().split("\\R");
+
+        for (String pair : pairs){
+            String key = pair.split(" ")[0];
+            String value = pair.split(" ")[1];
+            switch (key) {
+                case "Name:":
+                    profile.setName(value);
+                    break;
+                case "Age:":
+                    profile.setAge(Integer.parseInt(value));
+                    break;
+                case "Email:":
+                    profile.setEmail(value);
+                    break;
+                case "Phone:":
+                    profile.setPhone(Long.parseLong(value));
+                    break;
+                default:
+                    break;
+            }
+        }
+        return profile;
     }
 }
